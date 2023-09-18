a2ed3ea1ffe98482eb55d1c44ef8c89df6c23b95
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-09-18 16:25:37 +0200

    feat (test): add csv test and regex for joular data

--- src/test/java/com/snail/sentinel/backend/commons/UtilTest.java
+++ src/test/java/com/snail/sentinel/backend/commons/UtilTest.java
@@ -2,16 +2,19 @@ package com.snail.sentinel.backend.commons;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilTest {
 
-    /*@Test
+    @Test
     void readClassCsvToJsonTest() throws IOException {
-        String csvPath = System.getenv("TEST_CK_REPO_PATH") + Util.AST_ELEM_CLASS + ".csv";
+        String csvPath = "src/test/resources/ck-csv-test/" + Util.AST_ELEM_CLASS + ".csv";
 
         JSONObject line = new JSONObject();
         line.put(Util.FILE, "/home/jerome/Documents/Assistant/Recherche/open-source-repositories/commons-lang/src/test/java/org/apache/commons/lang3/concurrent/TimedSemaphoreTest.java");
@@ -26,7 +29,23 @@ class UtilTest {
 
         List<JSONObject> maybeTmpList = Util.readCsvToJson(csvPath);
         assertTrue(compareJSONLists(maybeTmpList, jsonObjectList));
-    }*/
+    }
+
+    @Test
+    void readCsvWithoutHeaderToJsonTest() throws IOException {
+        String csvPath = "src/test/resources/joular-csv-test/joular-test.csv";
+
+        JSONObject line1 = new JSONObject();
+        line1.put("org.springframework.boot.context.properties.bind.JavaBeanBinder$BeanProperties$$Lambda$4902/0x0000000802bbbc10.<init> -1", "0.0457");
+        JSONObject line2 = new JSONObject();
+        line2.put("org.springframework.boot.web.embedded.jetty.ForwardHeadersCustomizer.customize 34", "0.2357");
+        List<JSONObject> jsonObjectList = new ArrayList<>();
+        jsonObjectList.add(line1);
+        jsonObjectList.add(line2);
+
+        List<JSONObject> maybeTmpList = Util.readCsvWithoutHeaderToJson(csvPath);
+        assertTrue(compareJSONLists(maybeTmpList, jsonObjectList));
+    }
 
     private static boolean compareJSONLists(List<JSONObject> list1, List<JSONObject> list2) {
         if (list1.size() != list2.size()) {
