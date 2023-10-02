1ef1219846b6ddda3a4b5ab3f327288a73341bc5
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-10-02 15:05:11 +0200

    feat: project clone and ck working

--- scripts/docker-sentinel.sh
+++ scripts/docker-sentinel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd ..
-export JAVA_HOME=/usr/lib/jvm/java-19-openjdk-amd64
+#export JAVA_HOME=/usr/lib/jvm/java-19-openjdk-amd64
 export COMPOSE_PROJECT_NAME=sentinelbackend
 
 sudo ./mvnw clean
+#TODO supprimer token et le mettre en variable d'environnement
 export GITHUB_TOKEN=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
 export REPO_PATH=/home/jerome/Documents/Assistant/Recherche/open-source-repositories/
 export BATCH_SIZE=20000
