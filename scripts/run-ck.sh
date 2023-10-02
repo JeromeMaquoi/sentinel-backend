d299a9cd393ed53e957aa4cd152fca9a26947afb
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-10-02 15:05:11 +0200

    feat: project clone and ck working

--- scripts/run-ck.sh
+++ scripts/run-ck.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+cd /$REPO_DIR || exit
+echo "Current directory : $PWD"
+find "$PWD" -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
+    echo "Coucou $dir"
+    cd "$dir" || exit
+    mkdir "output-ck"
+    java -jar /plugins/ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar "$dir" False 0 True "$dir/output-ck/"
+done
--- scripts/sentinel-workflow.sh
+++ scripts/sentinel-workflow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 # Preparation of the data: cloning repositories, executing CK and Joular
-docker build -t sentinel-workflow --build-arg REPO_PATH_ARG=./open-source-repositories/ .
-docker run -it --name sentinel-workflow sentinel-workflow
+docker build -t sentinel-workflow --build-arg REPO_DIR_ARG=open-source-repositories .
+docker run -it --rm --name sentinel-workflow sentinel-workflow
 
 # Start sentinel-backend app to handle data gathered by CK and Joular into a MongoDB database
