965df90dfd0997b3bb9b0bd3ff6461f1b2d31cad
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-10-02 18:20:06 +0200

    feat: joular almost working

--- scripts/all-workflow.sh
+++ scripts/all-workflow.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 cd /scripts || exit
-# Run the clone and analysis scripts
+# Clone all the open source repositories
 ./clone-repos.sh
+
+# Execution of CK for each repository
 ./run-ck.sh
+
+# Execution of JoularJX for each repository
+#./run-joular.sh
+
+# Execution of sentinel-backend to store the data into the database
