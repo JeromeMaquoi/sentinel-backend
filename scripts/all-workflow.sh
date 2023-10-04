ac59e0265c90529ebacb83e6dca0da819edeedc3
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-10-04 15:17:00 +0200

    Update all-workflow.sh

--- scripts/all-workflow.sh
+++ scripts/all-workflow.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update && apt-get install -y openjdk-19-jdk maven gradle
 
-REPO_DIR="/open-source-repositories"
+export REPO_DIR="/open-source-repositories"
 # Clone all the open source repositories
 ./clone-repos.sh
 
