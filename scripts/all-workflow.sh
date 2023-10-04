f6fa0a1144fa59a6b36ca5b0c5931e0a2bd4c881
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-10-04 15:07:15 +0200

    Update all-workflow.sh

--- scripts/all-workflow.sh
+++ scripts/all-workflow.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cd /scripts || exit
+sudo apt-get update && apt-get install -y openjdk-19-jdk maven gradle
+
+REPO_DIR="/open-source-repositories"
 # Clone all the open source repositories
 ./clone-repos.sh
 
 # Execution of CK for each repository
-./run-ck.sh
+#./run-ck.sh
 
 # Execution of JoularJX for each repository
-#./run-joular.sh
+./run-joular.sh
 
 # Execution of sentinel-backend to store the data into the database
