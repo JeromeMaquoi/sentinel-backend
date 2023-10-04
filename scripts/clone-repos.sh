26b4c8daec681568fef13e7ca8488b3a18b573a7
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-10-04 15:39:57 +0200

    update

--- scripts/clone-repos.sh
+++ scripts/clone-repos.sh
@@ -6,6 +6,8 @@
 #cd spring-boot || exit
 #git reset --hard 3ed1f1a064a10e53adc2ad8c0b46a4b2c148ee21 # release v3.1.4
 
+cd $REPO_DIRECTORY || exit
+echo "$PWD"
 git clone https://github.com/apache/commons-lang ./commons-lang
 echo "Repository commons-lang cloned successfully!"
 cd commons-lang || exit
