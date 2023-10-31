825f81ae2ef905e41319f638a98b366dbe3cb7b6
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-10-31 16:54:57 +0100

    feat (SEN-6): add total time + working for all projects

--- scripts/all-workflow.sh
+++ scripts/all-workflow.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update && apt-get install -y openjdk-19-jdk openjdk-17-jdk openjdk-8-jdk maven gradle
+start=$(date +%s)
+
+sudo apt-get update && apt-get install -y openjdk-19-jdk openjdk-17-jdk maven gradle
 
 # Clone all the open source repositories
 sudo REPO_DIRECTORY="$REPO_DIRECTORY" bash ./clone-repos.sh
@@ -12,3 +14,11 @@ sudo REPO_DIRECTORY="$REPO_DIRECTORY" PLUGINS_DIRECTORY="$PLUGINS_DIRECTORY" bas
 sudo REPO_DIRECTORY="$REPO_DIRECTORY" PLUGINS_DIRECTORY="$PLUGINS_DIRECTORY" NB_ITERATION="$NB_ITERATION" bash ./run-joular.sh
 
 echo "All CK and joular data generated for all the projects !"
+
+# Execution of sentinel
+sudo REPO_DIRECTORY="$REPO_DIRECTORY" BATCH_SIZE="$BATCH_SIZE" GITHUB_TOKEN="$GITHUB_TOKEN" bash docker-sentinel.sh
+
+
+end=$(date +%s)
+diff=$((end-start))
+echo "Execution time: $diff seconds."
--- scripts/clone-repos.sh
+++ scripts/clone-repos.sh
@@ -5,7 +5,7 @@ cd "$REPO_DIRECTORY" || exit
 # -----------
 # spring-boot
 # -----------
-:'
+
 if [ ! -d "spring-boot" ]; then
     git clone https://github.com/spring-projects/spring-boot "$REPO_DIRECTORY/spring-boot"
     echo "Repository spring-boot cloned successfully!"
@@ -14,6 +14,7 @@ else
 fi
 cd "$REPO_DIRECTORY/spring-boot" || exit
 git reset --hard 3ed1f1a064a10e53adc2ad8c0b46a4b2c148ee21 # release v3.1.4
+echo -e "\n\n"
 
 # ------------
 # commons-lang
@@ -24,10 +25,10 @@ if [ ! -d "commons-lang" ]; then
 else
     echo "Repository already there!"
 fi
-
 cd "$REPO_DIRECTORY/commons-lang" || exit
 git reset --hard bcc10b359318397a4d12dbaef22b101725bc6323 # release v3.13.0
-'
+echo -e "\n\n"
+
 
 # ---------------------
 # commons-configuration
@@ -40,6 +41,7 @@ else
 fi
 cd "$REPO_DIRECTORY/commons-configuration" || exit
 git reset --hard 59e5152722198526c6ffe5361de7d1a6a87275c7
+echo -e "\n\n"
 
 #cd .. || exit
 #if [ ! -d "jabref" ]; then
