0177004a032d28ad53e8ff89b844da9a9fcb4e52
Jérôme Maquoi <jerome.maquoi@unamur.be>
2023-11-06 17:46:46 +0100

    Fix JAVA_HOME for production build

--- scripts/docker-sentinel.sh
+++ scripts/docker-sentinel.sh
@@ -6,6 +6,7 @@ exec &> >(tee -a "$LOG_FILE")
 start=$(date +%s)
 
 cd ..
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export COMPOSE_PROJECT_NAME=sentinelbackend
 sudo ./mvnw clean
 ./mvnw package -Pprod jib:dockerBuild
