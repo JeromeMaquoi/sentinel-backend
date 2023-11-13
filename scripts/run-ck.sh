c60dd22be2a9eb7201c542056c4a2054f3299dd3
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-11-13 14:14:46 +0100

    adding jabref almost working

--- scripts/run-ck.sh
+++ scripts/run-ck.sh
@@ -4,6 +4,7 @@ cd "$REPO_DIRECTORY" || exit
 find "$PWD" -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
     cd "$dir" || exit
     if [ ! -d "output-ck" ] || [ "$(ls -1A "$dir/output-ck/" | wc -l)" -eq 0 ]; then
+        echo -e "\nRunning CK for $dir"
         mkdir "output-ck"
         java -jar "$PLUGINS_DIRECTORY/ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar" "$dir" False 0 True "$dir/output-ck/"
     else
--- scripts/run-joular.sh
+++ scripts/run-joular.sh
@@ -15,13 +15,13 @@ cd "$REPO_DIRECTORY/commons-configuration" || exit
 # Update config.properties
 package_commons_configuration="filter-method-names=org.apache.commons.configuration2"
 sed -i "17s/.*/${package_commons_configuration}/" "$config_file"
-cp "$config_file" "$REPO_DIRECTORY/"commons-configuration
+cp "$config_file" "$REPO_DIRECTORY/commons-configuration"
 
 # Update pom.xml with joularjx plugin path
 build_maven_commons_configuration="$PLUGINS_DIRECTORY/commons-configuration/pom.xml"
 line_number=$(awk '/-javaagent/{print NR; exit}' "$build_maven_commons_configuration")
 sed -i "${line_number}s|-javaagent.*|-javaagent:${PLUGINS_DIRECTORY}/joularjx-2.0-modified.jar|" "$build_maven_commons_configuration"
-cp "$build_maven_commons_configuration" "$REPO_DIRECTORY/"commons-configuration
+cp "$build_maven_commons_configuration" "$REPO_DIRECTORY/commons-configuration"
 
 # Run tests with joular
 for ((i=1;i<=NB_ITERATION;i++))
@@ -46,7 +46,7 @@ cd "$REPO_DIRECTORY/commons-lang" || exit
 # Update config.properties
 package_commons_lang="filter-method-names=org.apache.commons.lang3"
 sed -i "17s/.*/${package_commons_lang}/" "$config_file"
-cp "$config_file" "$REPO_DIRECTORY/"commons-lang
+cp "$config_file" "$REPO_DIRECTORY/commons-lang"
 
 # Update pom.xml with joularjx plugin path
 build_maven_commons_lang="$PLUGINS_DIRECTORY/commons-lang/pom.xml"
@@ -65,6 +65,40 @@ done
 echo -e "\n\n\n\n"
 
 
+# ------
+# jabref
+# ------
+echo -e "------"
+echo -e "JABREF"
+echo -e "------"
+export JAVA_HOME=/usr/lib/jvm/jdk-21.0.1+12/
+cd "$REPO_DIRECTORY/jabref" || exit
+./gradlew -version
+
+# Update config.properties
+package_jabref="filter-method-names=org.jabref"
+sed -i "17s/.*/${package_jabref}/" "$config_file"
+cp "$config_file" "$REPO_DIRECTORY/jabref"
+
+# Update build.gradle with joularjx plugin path
+build_gradle="$PLUGINS_DIRECTORY/build.gradle"
+line_number=$(grep -n -- "-javaagent" "$build_gradle" | cut -d: -f1)
+sed -i "${line_number}s|-javaagent.*|-javaagent:${PLUGINS_DIRECTORY}/joularjx-2.0-modified.jar\"]|" "$build_gradle"
+cp "$build_gradle" "$REPO_DIRECTORY/jabref"
+
+# Run tests with joular
+sudo ./gradlew clean
+for ((i=1;i<=NB_ITERATION;i++))
+do
+    export ITERATION_ID=$i
+    echo -e "Start test for iteration $i\n"
+    sudo ./gradlew test
+    echo -e "Test for iteration $i done!\n\n"
+done
+echo -e "\n\n\n\n"
+
+
+:'
 # -----------
 # Spring-boot
 # -----------
@@ -92,7 +126,6 @@ line_number=$(grep -n -- "-javaagent" "$build_gradle" | cut -d: -f1)
 sed -i "${line_number}s|-javaagent.*|-javaagent:${PLUGINS_DIRECTORY}/joularjx-2.0-modified.jar\"|" "$build_gradle"
 cp "$build_gradle" "$REPO_DIRECTORY/spring-boot/spring-boot-project/spring-boot/"
 
-:'
 # Run tests with joular
 sudo ./gradlew clean
 for ((i=1;i<=NB_ITERATION;i++))
