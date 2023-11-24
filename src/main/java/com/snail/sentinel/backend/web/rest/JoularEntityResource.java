82907175c69538c9a70ccfdd4e7dce6e11a30e5d
JeromeMaquoi <jerome.maquoi@unamur.be>
2023-11-24 15:56:56 +0100

    add GET /api/v1/joular-entities

--- src/main/java/com/snail/sentinel/backend/web/rest/JoularEntityResource.java
+++ src/main/java/com/snail/sentinel/backend/web/rest/JoularEntityResource.java
@@ -1,7 +1,7 @@
 package com.snail.sentinel.backend.web.rest;
 
-import com.snail.sentinel.backend.domain.CkEntity;
-import com.snail.sentinel.backend.repository.CkEntityRepository;
+import com.snail.sentinel.backend.domain.JoularEntity;
+import com.snail.sentinel.backend.repository.JoularEntityRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -11,27 +11,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/api/v1")
-public class CkEntityResource {
-    private final Logger log = LoggerFactory.getLogger(CkEntityResource.class);
+public class JoularEntityResource {
+    private final Logger log = LoggerFactory.getLogger(JoularEntityResource.class);
+    private final JoularEntityRepository joularEntityRepository;
 
-    private static final String ENTITY_NAME = "ckEntity";
-
-    private final CkEntityRepository ckEntityRepository;
-
-    public CkEntityResource(CkEntityRepository ckEntityRepository) {
-        this.ckEntityRepository = ckEntityRepository;
+    public JoularEntityResource(JoularEntityRepository joularEntityRepository) {
+        this.joularEntityRepository = joularEntityRepository;
     }
 
-    @GetMapping("/ck-entities")
-    public Page<CkEntity> getAllCkEntities(
+    @GetMapping("/joular-entities")
+    public Page<JoularEntity> getAllJoularEntities(
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "100") int size
     ) {
-        log.debug("REST request to get all CkEntities");
+        log.debug("REST request to get all JoularEntities");
         PageRequest pageRequest = PageRequest.of(page, size);
-        return ckEntityRepository.findAll(pageRequest);
+        return joularEntityRepository.findAll(pageRequest);
     }
 }
