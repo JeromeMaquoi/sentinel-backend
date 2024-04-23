1b9e1f7e5f07fb4bda92b088bd88d621b0864592
JeromeMaquoi <jerome.maquoi@unamur.be>
2024-04-23 10:16:33 +0200

    Remove joular-node-entity front

--- src/main/webapp/app/entities/joular-node-entity/joular-node-entity.model.ts
+++
@@ -1,7 +0,0 @@
-export interface IJoularNodeEntity {
-  id: string;
-  lineNumber?: number | null;
-  value?: number | null;
-}
-
-export type NewJoularNodeEntity = Omit<IJoularNodeEntity, 'id'> & { id: null };
