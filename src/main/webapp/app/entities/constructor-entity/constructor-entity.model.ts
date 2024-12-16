b435f883f1e449e98e13243efe8b2be43708a81e
JeromeMaquoi <jerome.maquoi@unamur.be>
2024-12-16 14:01:44 +0100

    Removing ConstructorEntity and AttributeEntity from webapp folder

--- src/main/webapp/app/entities/constructor-entity/constructor-entity.model.ts
+++
@@ -1,9 +0,0 @@
-export interface IConstructorEntity {
-  id: string;
-  name?: string | null;
-  signature?: string | null;
-  pkg?: string | null;
-  file?: string | null;
-}
-
-export type NewConstructorEntity = Omit<IConstructorEntity, 'id'> & { id: null };
