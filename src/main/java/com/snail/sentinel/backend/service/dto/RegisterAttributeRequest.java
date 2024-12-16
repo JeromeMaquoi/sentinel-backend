43c4cf38c66354a9eabde4f7c25a7e46d94dec6d
JeromeMaquoi <jerome.maquoi@unamur.be>
2024-12-16 11:38:02 +0100

    Add attributes to the DTO for the creation of a new ConstructorEntity object

--- src/main/java/com/snail/sentinel/backend/service/dto/RegisterAttributeRequest.java
+++ src/main/java/com/snail/sentinel/backend/service/dto/RegisterAttributeRequest.java
@@ -2,6 +2,9 @@ package com.snail.sentinel.backend.service.dto;
 
 public class RegisterAttributeRequest {
     private String constructorSignature;
+    private String constructorName;
+    private String constructorClassName;
+    private String constructorFileName;
     private String attributeName;
     private String attributeType;
 
@@ -27,5 +30,29 @@ public class RegisterAttributeRequest {
 
     public void setAttributeType(String attributeType) {
         this.attributeType = attributeType;
+    }
+
+    public String getConstructorName() {
+        return constructorName;
+    }
+
+    public void setConstructorName(String constructorName) {
+        this.constructorName = constructorName;
+    }
+
+    public String getConstructorClassName() {
+        return constructorClassName;
+    }
+
+    public void setConstructorClassName(String constructorClassName) {
+        this.constructorClassName = constructorClassName;
+    }
+
+    public String getConstructorFileName() {
+        return constructorFileName;
+    }
+
+    public void setConstructorFileName(String constructorFileName) {
+        this.constructorFileName = constructorFileName;
     }
 }
