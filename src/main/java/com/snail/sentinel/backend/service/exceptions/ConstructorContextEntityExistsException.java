046ce436b1c87b4f0116ccc29fbfdc14e6f6d8f8
JeromeMaquoi <33390494+JeromeMaquoi@users.noreply.github.com>
2025-12-12 15:40:14 +0100

    Update ConstructorContextEntityExistsException.java

--- src/main/java/com/snail/sentinel/backend/service/exceptions/ConstructorContextEntityExistsException.java
+++ src/main/java/com/snail/sentinel/backend/service/exceptions/ConstructorContextEntityExistsException.java
@@ -1,5 +1,9 @@
 package com.snail.sentinel.backend.service.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class ConstructorContextEntityExistsException extends RuntimeException {
     public ConstructorContextEntityExistsException(String message) {
         super(message);
